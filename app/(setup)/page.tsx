2337d1a9855588739048563ecabf2ae62ca5d4ce
EskinderAbera <eske17.ea@gmail.com>
2023-08-30 17:44:39 +0300

    add create server form

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -2,6 +2,7 @@ import { redirect } from "next/navigation";
 import { db } from "@/lib/db";
 import { initialProfile } from "@/lib/initial-profile";
 import React from "react";
+import InitialModal from "@/components/modals/initial-modal";
 
 export default async function SetupPage() {
   const profile = await initialProfile();
@@ -19,5 +20,5 @@ export default async function SetupPage() {
     return redirect(`/servers/${server.id}`);
   }
 
-  return <div>Create a server</div>;
+  return <InitialModal />;
 }
